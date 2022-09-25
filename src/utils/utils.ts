334760729945cf36484d12c1dbdd9623eb08cddd
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-25 23:05:45 +0530

    fix: use undici request as it's faster

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import type { APIMessageComponentEmoji, Emoji } from "discord.js";
+import { request } from "undici";
 import twemoji from "twemoji";
-import axios from "axios";
 
 export function formatBytes(bytes: number, decimals = 2) {
 	if (bytes === 0) return "0 Bytes";
@@ -24,14 +24,13 @@ export function parseDiscordEmoji(emoji: Emoji | APIMessageComponentEmoji) {
 }
 
 export async function downloadImageToDataURL(url: string): Promise<string | null> {
-	const response = await axios
-		.get(url, {
-			responseType: "arraybuffer",
-			validateStatus: (status) => status >= 200 && status < 300,
-		})
+	const response = await request(url);
+
+	const dataURL = await response.body
+		.arrayBuffer()
 		.then((res) => {
-			const data = Buffer.from(res.data, "binary").toString("base64");
-			const mime = res.headers["content-type"];
+			const data = Buffer.from(res).toString("base64");
+			const mime = response.headers["content-type"];
 
 			return `data:${mime};base64,${data}`;
 		})
@@ -43,5 +42,5 @@ export async function downloadImageToDataURL(url: string): Promise<string | null
 			return null;
 		});
 
-	return response;
+	return dataURL;
 }
