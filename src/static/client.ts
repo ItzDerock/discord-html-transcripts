edb233b2ca008c7cda1eed5019eb7b2ce72ee683
Derock <derock@derock.dev>
2022-09-28 20:14:10 -0400

    feat: highlight message on reply click

--- src/static/client.ts
+++ src/static/client.ts
@@ -1,20 +1,27 @@
+// TODO: create some sort of build system to compile this file
+
 /*
 // whenever user clicks on element with data-goto attribute, scroll to that message
 document.addEventListener('click', (e) => {
   const target = e.target;
-  const goto = target.getAttribute('data-goto');
+  if(!target) return;
+
+  const goto = target?.getAttribute('data-goto');
+
   if (goto) {
-    const message = document.getElementById(`m-${goto}`);
+    const message = document.getElementById(`m-\${goto}`);
     if (message) {
-      message.scrollIntoView({ behavior: 'smooth' });
+      message.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      message.style.backgroundColor = 'rgba(148, 156, 247, 0.1)';
+      message.style.transition = 'background-color 0.5s ease';
+      setTimeout(() => {
+        message.style.backgroundColor = 'transparent';
+      }, 1000);
+    } else {
+      console.warn(`Message \${goto} not found.`);
     }
   }
 });
 */
 export const scrollToMessage =
-  'document.addEventListener("click",t=>{const e=t.target.getAttribute("data-goto");if(e){const t=document.getElementById(`m-${e}`);t&&t.scrollIntoView({behavior:"smooth"})}});';
-
-/*
-wait for 
-
-*/
+  'document.addEventListener("click",t=>{let e=t.target;if(!e)return;let o=e?.getAttribute("data-goto");if(o){let r=document.getElementById(`m-${o}`);r?(r.scrollIntoView({behavior:"smooth",block:"center"}),r.style.backgroundColor="rgba(148, 156, 247, 0.1)",r.style.transition="background-color 0.5s ease",setTimeout(()=>{r.style.backgroundColor="transparent"},1e3)):console.warn("Message ${goto} not found.")}});'
