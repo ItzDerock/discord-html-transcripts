dbd29b7e4c395f25ddb7b339142858b56f393d7e
Derock <derock@derock.dev>
2023-01-24 21:48:09 -0500

    fix: responsiveness

--- src/generator/index.tsx
+++ src/generator/index.tsx
@@ -100,7 +100,7 @@ export default async function renderMessages({ messages, channel, callbacks, ...
     <html>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/* favicon */}
         <link
